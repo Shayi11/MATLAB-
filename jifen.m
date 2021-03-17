dbc8d5352eef47287ea62e81a7822def2d086093
Shayi11 <55230503+Shayi11@users.noreply.github.com>
2021-03-17 16:04:22 +0800

    Add files via upload

---
+++ jifen.m
@@ -0,0 +1,43 @@
+function [Output1,Output2] = jifen(Input1,Input2)
+%jifen=计分 
+%   就是计算每个站点的得分
+vi0 = Input1; % vi0是站点的三个参数，仅有三个哦~
+sxxian=Input2;
+
+%实现vij,P1,P2的计算
+for m=1:10
+    for n=1:3
+        vij(m,n)=abs(sxxian(m,2*n)-sxxian(m,2*n-1));
+        P1ij(m,n)=abs(vi0(n)-0.5*(sxxian(m,2*n)+sxxian(m,2*n-1)))-0.5*(sxxian(m,2*n)-sxxian(m,2*n-1));
+        P2ij(n)=abs(vi0(n)-0.5*sxxian(end,2*n))-0.5*sxxian(end,2*n);
+    end
+end
+
+%计算关联系数Kij
+for m=1:10
+    for n=1:3
+        if vi0(n)>=sxxian(m,2*n-1) && vi0(n)<=sxxian(m,2*n)
+            Kij(m,n)=-(P1ij(m,n)/vij(m,n));
+        else
+            if P1ij(m,n)~=0
+                Kij(m,n)=P1ij(m,n)/(P2ij(n)-P1ij(m,n));
+            else P1ij(m,n)==0
+                Kij(m,n)=-P1ij(m,n)-1;
+            end
+        end
+    end
+end
+
+%确定综合关联度Kj
+for m=1:10
+    Kj(m)=0.2493*Kij(m,1)+0.1571*Kij(m,2)+0.5936*Kij(m,3);
+end
+
+%确定出评分
+[maxzhi,hang]=max(Kj);
+
+Output1=maxzhi;
+Output2=hang;
+
+end
+
